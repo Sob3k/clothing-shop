cadd9b1f07a4f871d1613c9b39f5a09c8b6f82eb
Sob3k <sebastian977@poczta.fm>
2020-05-15 22:06:34 +0200

    Added collections overiew and stripe test payments

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,11 +3,12 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from "reselect";
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems,total}) => (
    <div className="checkout-page">
-       <div class="checkout-header">
+       <div className="checkout-header">
            <div className="header-block">
                <span>Product</span>
            </div>
@@ -32,6 +33,12 @@ const CheckoutPage = ({cartItems,total}) => (
        <div className="total">
             <span>TOTAL: ${total}</span>
        </div>
+       <div className='test-warning'>
+           *Please use the following test credit card for payments*
+           <br/>
+           4242 4242 4242 4242 - Exp: Any future date - CVV: Any 3 digits
+       </div>
+       <StripeCheckoutButton price={total} />
    </div>
 )
 
